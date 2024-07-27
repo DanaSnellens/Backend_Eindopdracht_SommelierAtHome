42c1e990e387c2241636efa6a83dfbd68def1235
DanaSnellens <danasnellens@gmail.com>
2024-07-27 21:05:36 +0200

    WineAdvice model zonder relaties gemaakt

--- src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/models/WineAdvice.java
+++ src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/models/WineAdvice.java
@@ -1,23 +1,31 @@
 package nl.novi.eindopdr_danasnellens_sommelierathome.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.File;
+import java.util.List;
+
 @Entity
+@Table(name = "WineAdvices")
 @Getter
 @Setter
 public class WineAdvice {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter(AccessLevel.NONE)               //KLOPT DIT?
     private Long id;
 
-    private String advice;
+    //relaties
+        // WineList<Wine>
+        // Sommelier
+        // Client
+        // WineAdviceRequest
+        // WineList
 
     private String personalMessage;
-
+/*    private List<Wine> wines;*/
+    private File adviceExplanation;
 }
