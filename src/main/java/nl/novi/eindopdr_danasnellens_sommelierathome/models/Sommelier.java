73a11f317ade3dfe33d2673b0219c90d5f63138c
DanaSnellens <danasnellens@gmail.com>
2024-07-29 00:48:52 +0200

    User Model aangemaakt, sommelier & client nog niet (want hoe zit dit met authenticatie???)

---
+++ src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/models/Sommelier.java
@@ -0,0 +1,4 @@
+package nl.novi.eindopdr_danasnellens_sommelierathome.models;
+
+public class Sommelier extends User{
+}
--- src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/models/User.java
+++ src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/models/User.java
@@ -1,14 +1,24 @@
 package nl.novi.eindopdr_danasnellens_sommelierathome.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Table(name = "Users")
+@Getter
+@Setter
 public abstract class User {
     @Id
-    @GeneratedValue
-    Long id;                //Hier geen private voor?
-    private String surName;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;                //Hier geen private voor?
+
+    //relaties
+
+    private String firstName;
     private String lastName;
     private String email;
+    private String password;
 }
