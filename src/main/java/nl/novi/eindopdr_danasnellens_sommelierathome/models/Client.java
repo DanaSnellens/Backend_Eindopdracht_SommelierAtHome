a9bf65e89ba56a034f775766002c2f217af3239a
DanaSnellens <danasnellens@gmail.com>
2024-08-31 01:08:10 +0200

    Client (sub)class toegevoegd incl enum (maar meot deze wel hier gedefinieerd worden of apart? Zie TO-DO

---
+++ src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/models/Client.java
@@ -0,0 +1,22 @@
+package nl.novi.eindopdr_danasnellens_sommelierathome.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+public class Client extends User {
+
+    //TODO: moet de enum hier of in een aparte enummeration en waar dan (model? en dan ook in de andere lagen?)? Hoe hier noteren? https://www.baeldung.com/spring-boot-enum-mapping
+    private enum Membership {
+        NONE,
+        BASIC,
+        REGULAR,
+        PREMIUM,
+    }
+
+}
