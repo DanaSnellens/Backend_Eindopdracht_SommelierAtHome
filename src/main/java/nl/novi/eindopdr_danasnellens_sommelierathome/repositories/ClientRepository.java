40b9e76ef2bfa445a0132dfc5aa2282ca90f814d
DanaSnellens <danasnellens@gmail.com>
2024-08-31 02:04:43 +0200

    Sommelier (sub)class + Sommelier- & ClientRepository toegevoegd

---
+++ src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+package nl.novi.eindopdr_danasnellens_sommelierathome.repositories;
+
+import nl.novi.eindopdr_danasnellens_sommelierathome.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
---
+++ src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/repositories/SommelierRepository.java
@@ -0,0 +1,5 @@
+package nl.novi.eindopdr_danasnellens_sommelierathome.repositories;
+
+public interface SommelierRepository extends JpaRepository<Sommelier, Long> {
+
+}
