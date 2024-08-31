e2dc1b90de2e457946d909ee1232611e7f2646e6
DanaSnellens <danasnellens@gmail.com>
2024-08-31 22:40:55 +0200

    Abstracte UserService toegevoegd

--- src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/services/UserService.java
+++ src/main/java/nl/novi/eindopdr_danasnellens_sommelierathome/services/UserService.java
@@ -9,53 +9,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+//TODO: Klopt dit gebruik van T?
 @Service
-public class UserService {
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
+public abstract class UserService <T extends User> {
+//TODO DIT HIER NIET NOEMEN? (volgens chatGPT)    private final UserRepository userRepository;
+/*    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
+    }*/
 
-    // Get All
-    public List<UserOutputDto> getAllUsers() {
-        List<User> userList = userRepository.findAll();
-        return userModelListToOutputList(userList);
-    }
+    //Get All
+    public abstract List<T> getAllUsers();
 
-    // Get One
-    public UserOutputDto getUserByUsername(String userName) {
-        Optional<User> optionalUser = userRepository.findByUserName(userName);
-        if (optionalUser.isPresent()) {
-            return userFromModelToOutputDto(optionalUser.get());
-        }
-        else throw new UsernameNotFoundException("No user found with username: " + userName);
-    }
+    //Get One
+    //TODO zowel getUserByUserName als by ID?
+    public abstract Optional<T> getUserByUsername(String userName);
 
-    // Create
-    //@AuthenticationPrincipal UserDetails userDetails nog fixen (ook in controller). Zie huiswerkklas 16; 52 minuten
+    public abstract Optional<T> getUserById(Long id);
 
-    public UserOutputDto createUser(UserInputDto userInputDto/*, String userName */ ) {
-        User u = userRepository.save(userFromInputDtoToModel(userInputDto/*, userName*/));
-        return userFromModelToOutputDto(u);
-    }
+    //Create
+    public abstract T addUser(T user);
 
-    // Update
+    //Update
+    public abstract T updateUser(Long id, T user);
 
-    public UserOutputDto updateUser (String userName, UserIputDto updatedUser) {
-        Optional<User> optionalUser = userRepository.findByUserName(userName);
-        if (optionalUser.isPresent()) {
-            return userFromModelToOutputDto(optionalUser.get());
-        }
-        else throw new UsernameNotFoundException("No user found with username: " + userName);
-    }
-
-    // Delete
-    public void deleteUserByUserName(String userName) {
-        Optional<User> optionalUser = userRepository.findByUserName(userName);
-        if (optionalUser.isPresent()) {
-            userRepository.deleteByUserName(userName);
-        }
-        else throw new UsernameNotFoundException("No user found with username: " + userName);
-    }
+    //Delete
+    public abstract void deleteUserById(Long id);
 }
+
+
